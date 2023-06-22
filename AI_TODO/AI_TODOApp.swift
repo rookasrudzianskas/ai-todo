7f9153f7adc0bf42808d68a9c52cf1776201e1d9
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 15:50:20 +0300

    initi

---
+++ AI_TODO/AI_TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_TODOApp.swift
+//  AI_TODO
+//
+//  Created by Rookas Rudzianskas on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI_TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AI_TODO
+//
+//  Created by Rookas Rudzianskas on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
