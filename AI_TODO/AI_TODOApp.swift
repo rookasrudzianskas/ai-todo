d406321f24c00b6e60a99d351b82a6057f96cab2
Rokas <rookas.rudzenskas@gmail.com>
2023-06-22 15:54:13 +0300

    🔧 chore(AI_TODOApp.swift): add FirebaseApp.configure() to the app's initializer to configure Firebase SDK 🚀 feat(AI_TODO.xcodeproj): update UserInterfaceState.xcuserstate binary file to reflect changes in the app's UI state after Firebase SDK integration

--- AI_TODO/AI_TODOApp.swift
+++ AI_TODO/AI_TODOApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AI_TODOApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
